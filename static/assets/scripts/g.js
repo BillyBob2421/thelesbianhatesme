4cf36150f9035981aed6135fe467a44bcfb736f0
BillyBob2421 <162757129+BillyBob2421@users.noreply.github.com>
2024-03-08 16:19:52 -0600

    Update g.js

--- static/assets/scripts/g.js
+++ static/assets/scripts/g.js
@@ -27,10 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const btn = document.createElement('button')
         btn.appendChild(pinIcon)
         btn.style.float = 'right'
-        btn.style.backgroundColor = 'rgb(45,45,45)'
+        btn.style.backgroundColor = 'rgb(90,200,45)'
         btn.style.borderRadius = '50%'
         btn.style.borderColor = 'transparent'
-        btn.style.color = 'white'
+        btn.style.color = 'orange'
         btn.style.top = '-200px'
         btn.style.position = 'relative'
         btn.onclick = function () {
