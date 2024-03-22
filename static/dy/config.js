600c8a71264e8e8bdb84b2153671136a9683c1cf
BillyBob2421 <162757129+BillyBob2421@users.noreply.github.com>
2024-03-21 23:26:47 -0500

    Update config.js

--- static/dy/config.js
+++ static/dy/config.js
@@ -1,27 +1,26 @@
 self.__dynamic$config = {
   prefix: '/a/q/',
   encoding: 'xor',
-  mode: 'production', 
-  logLevel: 0, 
+  mode: 'production',
+  logLevel: 0,
   bare: {
-    version: 3, 
-    path: '/v/',
+    version: 2,
+    path: '/o/',
   },
   tab: {
     title: null,
     icon: null,
-    ua: null,
-  },  
+    ua: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.3'
+  },
   assets: {
     prefix: '/dy/',
     files: {
-      handler: 'handler.js',
-      client: 'client.js',
-      worker: 'worker.js',
-      config: 'config.js',
-      inject: null,
+      handler: 'handler.js?v=4',
+      client: 'client.js?v=4',
+      worker: 'worker.js?v=4',
+      config: 'config.js?v=4',
+      inject: ''
     }
   },
-  block: [
-  ]
+  block: []
 };
