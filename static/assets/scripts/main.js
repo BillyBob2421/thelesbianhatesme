114f4c25bb6692e031a6706b3538b7d85b9ed7ec
BillyBob2421 <162757129+BillyBob2421@users.noreply.github.com>
2024-04-22 12:47:10 -0500

    Update main.js

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,21 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Ads
-  if (localStorage.getItem('ad') === null || localStorage.getItem('ad') === 'default') {
-    localStorage.setItem('ad', 'on')
-  }
+    localStorage.setItem('ad', 'off');
 
-  var advDiv = document.getElementById('adv')
-  if (advDiv && localStorage.getItem('ad') === 'default') {
-    var script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = '//raggedstriking.com/4a/60/13/4a60134b5ce9b51b61acf6084265ecfd.js'
-    advDiv.appendChild(script)
-    console.log('Script inserted inside the adv div.')
-  } else if (advDiv && localStorage.getItem('ad') === 'off') {
-    advDiv.remove()
-    console.log('The adv div has been removed.')
-  }
-})
+    var advDiv = document.getElementById('adv');
+    if (advDiv && localStorage.getItem('ad') === 'on') {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = '//oysterscoldtiny.com/1c/c3/8a/1cc38a6899fdf8ba4dfe779bcc54627b.js';
+        advDiv.appendChild(script);
+        console.log('Script inserted inside the adv div.');
+    } else if (advDiv && localStorage.getItem('ad') === 'off') {
+        advDiv.remove();
+        console.log('The adv div has been removed.');
+    }
+});
+
 
 // Nav
 var nav = document.querySelector('.fixed-nav-bar')
