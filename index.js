3bfd10652ae0b90405d319e7ee35cd7517768b55
BillyBob2421 <162757129+BillyBob2421@users.noreply.github.com>
2024-05-02 18:26:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const __dirname = process.cwd()
 const server = http.createServer()
 const app = express()
 const bareServer = createBareServer("/o/")
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 443
 
 if (config.challenge) {
   console.log(`Password protection is enabled. Usernames are: ${Object.keys(config.users)}`)
