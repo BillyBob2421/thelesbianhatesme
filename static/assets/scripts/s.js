e8c3eb3dc76a627ec13730dc2579dbc4b2cec134
BillyBob2421 <162757129+BillyBob2421@users.noreply.github.com>
2024-05-09 22:48:56 -0500

    Update s.js

--- static/assets/scripts/s.js
+++ static/assets/scripts/s.js
@@ -262,56 +262,66 @@ function themeChange(ele) {
   window.location = window.location
 }
 // AB Cloak
-try {
+function AB() {
+  let inFrame
+
+  try {
     inFrame = window !== top
-} catch (e) {
+  } catch (e) {
     inFrame = true
+  }
+
+  if (!inFrame && !navigator.userAgent.includes("Firefox")) {
+    const popup = open("about:blank", "_blank")
+    if (!popup || popup.closed) {
+      alert("Please allow popups and redirects.")
+    } else {
+      const doc = popup.document
+      const iframe = doc.createElement("iframe")
+      const style = iframe.style
+      const link = doc.createElement("link")
+
+      const name = localStorage.getItem("name") || "My Drive - Google Drive"
+      const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png"
+
+      doc.title = name
+      link.rel = "icon"
+      link.href = icon
+
+      iframe.src = location.href
+      style.position = "fixed"
+      style.top = style.bottom = style.left = style.right = 0
+      style.border = style.outline = "none"
+      style.width = style.height = "100%"
+
+      doc.head.appendChild(link)
+      doc.body.appendChild(iframe)
+
+      const pLink = localStorage.getItem(encodeURI("pLink")) || "https://www.nasa.gov/"
+      location.replace(pLink)
+
+      const script = doc.createElement("script")
+      script.textContent = `
+        window.onbeforeunload = function (event) {
+          const confirmationMessage = 'Leave Site?';
+          (event || window.event).returnValue = confirmationMessage;
+          return confirmationMessage;
+        };
+      `
+      doc.head.appendChild(script)
+    }
+  }
 }
 
-if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-    const ab = localStorage.getItem("ab")
-    if (ab === "true") {
-        const popup = open("about:blank", "_blank")
-        if (!popup || popup.closed) {
-            alert("Please allow popups and redirects.")
-        } else {
-            const doc = popup.document
-            const iframe = doc.createElement("iframe")
-            const style = iframe.style
-            const link = doc.createElement("link")
-
-            const name = localStorage.getItem("name") || "My Drive - Google Drive"
-            const icon = localStorage.getItem("icon") || "https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png"
-
-            doc.title = name
-            link.rel = "icon"
-            link.href = icon
-
-            iframe.src = location.href
-            style.position = "fixed"
-            style.top = style.bottom = style.left = style.right = 0
-            style.border = style.outline = "none"
-            style.width = style.height = "100%"
-
-            doc.head.appendChild(link)
-            doc.body.appendChild(iframe)
-
-            const pLink = localStorage.getItem(encodeURI("pLink")) || "https://bible-nextjs-app.vercel.app/"
-            location.replace(pLink)
-
-            const script = doc.createElement("script")
-            script.textContent = `
-                window.onbeforeunload = function (event) {
-                    const confirmationMessage = 'Leave Site?';
-                    (event || window.event).returnValue = confirmationMessage;
-                    return confirmationMessage;
-                };
-            `
-            doc.head.appendChild(script)
-        }
-    } else {
-        // Toggle is OFF, do nothing
-    }
+function toggleAB() {
+  ab = localStorage.getItem("ab")
+  if (ab == null) {
+    localStorage.setItem("ab", "true")
+  } else if (ab == "false") {
+    localStorage.setItem("ab", "true")
+  } else {
+    localStorage.setItem("ab", "false")
+  }
 }
 // Search Engine
 function EngineChange(dropdown) {
